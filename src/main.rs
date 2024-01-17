4df665f6c893503e0008f340af9d246ea1890299
Alden Keefe Sampson <aldenkeefesampson@gmail.com>
2024-01-16 22:26:40 -0500

    Download & decompress -- gross code but fast
    TODO: get better at rust error handling

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,80 @@
-use std::error::Error;
 use std::ffi::c_void;
+use std::{error::Error, num::TryFromIntError};
 
 use blosc2_src::{
     blosc1_cbuffer_sizes, blosc2_create_dctx, blosc2_decompress_ctx, blosc2_free_ctx,
     BLOSC2_DPARAMS_DEFAULTS,
 };
 use half::f16;
-// use futures::stream::StreamExt;
-// use itertools::iproduct;
+use itertools::{iproduct, Itertools};
 
-const ZARR_URL: &str = "https://zarr.world/hrrr-analysis-TMPonly-2023-06-chunks360x240x240.zarr";
+const HRRR_2023_06_TMP_URL: &str =
+    "https://zarr.world/hrrr-analysis-TMPonly-2023-06-chunks360x240x240.zarr";
+type ElementType = f16;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let mut set = tokio::task::JoinSet::new();
     let original_client = reqwest::Client::new();
 
-    let compressed_bytes = get_chunk_bytes(original_client.clone(), 1, 1, 1).await?;
+    iproduct!(0..2, 0..5, 0..8)
+        .map(|(x, y, z)| {
+            let client = original_client.clone(); // clone a copy which can be `move`d into future
+            set.spawn(get_chunk_partial_mean_unwrapped(client, x, y, z));
+        })
+        .collect_vec();
 
-    let chunk_values = decompress_chunk(&compressed_bytes)?;
+    let mut sum: f64 = 0.;
+    let mut count: u64 = 0;
+    while let Some(Ok(partial_mean)) = set.join_next().await {
+        sum += partial_mean.sum;
+        count += partial_mean.count;
+        // dbg!(partial_mean);
+    }
+    dbg!(sum, count, sum / count as f64);
 
-    let chunk_sum = chunk_values
-        .iter()
-        .cloned()
-        .fold(0 as f64, |sum, x| sum + f64::from(x));
-    let chunk_mean = chunk_sum / chunk_values.len() as f64;
-    dbg!(chunk_mean);
-
-    return Ok(());
-
-    // let futures = iproduct!(0..2, 0..5, 0..8).map(|(x, y, z)| {
-    //     let client = original_client.clone(); // clone a copy which can be `move`d into future
-    //     async move { get_chunk_bytes(client, x, y, z) }
-    // });
-
-    // let results = futures::stream::iter(futures)
-    //     .buffer_unordered(1000) // max n concurrent requests
-    //     .collect::<Vec<_>>() // collect results into a vector
-    //     .await;
-
-    // dbg!(results.len());
-
-    // Ok(())
+    Ok(())
 }
 
-fn decompress_chunk(compressed_bytes: &bytes::Bytes) -> Result<Vec<f16>, Box<dyn Error>> {
-    let (decompressed_len, compressed_len, _) = blosc_buffer_sizes(compressed_bytes);
+#[derive(Debug)]
+struct PartialMean {
+    sum: f64,
+    count: u64,
+}
+
+async fn get_chunk_partial_mean_unwrapped(
+    client: reqwest::Client,
+    x: u64,
+    y: u64,
+    z: u64,
+) -> PartialMean {
+    let compressed_bytes = get_chunk_bytes(client, HRRR_2023_06_TMP_URL, x, y, z)
+        .await
+        .expect("get bytes error");
+
+    let res = tokio::task::spawn_blocking(move || {
+        let values = decompress_chunk(&compressed_bytes)?;
+        let sum = values
+            .iter()
+            .cloned()
+            .fold(0 as f64, |sum, x| sum + f64::from(x));
+
+        let count = values.len().try_into()?;
+
+        Ok::<PartialMean, TryFromIntError>(PartialMean { sum, count })
+    })
+    .await;
+
+    res.expect("join error").expect("more")
+}
+
+fn decompress_chunk(compressed_bytes: &bytes::Bytes) -> Result<Vec<ElementType>, TryFromIntError> {
+    let (decompressed_len, _compressed_len, _) = blosc_buffer_sizes(compressed_bytes);
 
     // Allocate buffer to decompress into
-    let value_size = std::mem::size_of::<f16>();
-    let values = vec![f16::NAN; decompressed_len / value_size];
+    let value_size = std::mem::size_of::<ElementType>();
+    assert_eq!(decompressed_len % value_size, 0);
+    let values = vec![ElementType::NAN; decompressed_len / value_size];
 
     let actual_decompressed_len_or_err_code = unsafe {
         let decompress_context = blosc2_create_dctx(BLOSC2_DPARAMS_DEFAULTS);
@@ -57,28 +83,35 @@ fn decompress_chunk(compressed_bytes: &bytes::Bytes) -> Result<Vec<f16>, Box<dyn
             compressed_bytes.as_ptr() as *const c_void,
             compressed_bytes.len().try_into()?,
             values.as_ptr() as *mut c_void,
-            (values.len() * value_size).try_into()?,
+            // The max number of bytes to write to `values` -- especially dangerous if this is incorrect.
+            // This expression is equal to `decompressed_bytes` but calc it from the values vec explicitly
+            // to reduce the chance a later change in vec allocating above could introduce a bug.
+            (values.len() * std::mem::size_of_val(&values[0])).try_into()?,
         );
         blosc2_free_ctx(decompress_context);
         actual_decompressed_len_or_err_code
     };
 
-    if actual_decompressed_len_or_err_code != i32::try_from(decompressed_len)? {
-        return Err(format!(
-            "Decompression error code {}",
-            actual_decompressed_len_or_err_code
-        )
-        .into());
-    } else {
-        let decompressed_mb = decompressed_len as f64 / (1000 * 1000) as f64;
-        let compressed_mb = compressed_len as f64 / (1000 * 1000) as f64;
-        println!(
-            "Decompressed to {:.1}mb from {:.1}mb ({:.1}x compression)",
-            decompressed_mb,
-            compressed_mb,
-            decompressed_mb / compressed_mb
-        )
-    }
+    assert!(
+        actual_decompressed_len_or_err_code
+            == i32::try_from(decompressed_len).expect("decompressed len too long for i32")
+    );
+    // if actual_decompressed_len_or_err_code != i32::try_from(decompressed_len)? {
+    //     return Err(format!(
+    //         "Decompression error code {}",
+    //         actual_decompressed_len_or_err_code
+    //     )
+    //     .into());
+    // } else {
+    //     let decompressed_mb = decompressed_len as f64 / (1000 * 1000) as f64;
+    //     let compressed_mb = _compressed_len as f64 / (1000 * 1000) as f64;
+    //     println!(
+    //         "Decompressed to {:.1}mb from {:.1}mb ({:.1}x compression)",
+    //         decompressed_mb,
+    //         compressed_mb,
+    //         decompressed_mb / compressed_mb
+    //     )
+    // }
 
     Ok(values)
 }
@@ -99,17 +132,19 @@ fn blosc_buffer_sizes(compressed_bytes: &bytes::Bytes) -> (usize, usize, usize) 
 
 async fn get_chunk_bytes(
     client: reqwest::Client,
-    x: i64,
-    y: i64,
-    z: i64,
+    url: &str,
+    x: u64,
+    y: u64,
+    z: u64,
 ) -> Result<bytes::Bytes, reqwest::Error> {
     let chunk_str = format!("{}.{}.{}", x, y, z);
     println!("starting {}...", chunk_str);
 
     let chunk_bytes = client
-        .get(format!("{}/TMP/{}", ZARR_URL, chunk_str))
+        .get(format!("{}/TMP/{}", url, chunk_str))
         .send()
         .await?
+        .error_for_status()?
         .bytes()
         .await?;
 
